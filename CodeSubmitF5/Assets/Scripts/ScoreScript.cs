62afe54a46c98a9f58c34ab39d4162efd477e9ad
Noelia Barranco Godoy <noebg2009@outlook.es>
2023-03-10 17:57:47 +0100

    HealthBar y Score hechos

---
+++ CodeSubmitF5/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    [SerializeField]
+    private Text scoreText;
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
