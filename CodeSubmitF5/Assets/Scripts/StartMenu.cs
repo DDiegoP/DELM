695b6c00ca9a4e9ad44981036fa5b2e4e08722c9
DDiegoP <95857593+DDiegoP@users.noreply.github.com>
2023-03-11 11:23:40 +0100

    BotonSounds

--- CodeSubmitF5/Assets/Scripts/StartMenu.cs
+++ CodeSubmitF5/Assets/Scripts/StartMenu.cs
@@ -6,27 +6,34 @@ using UnityEngine.EventSystems;
 
 public class StartMenu : MonoBehaviour
 {
+    AudioSource audioData;
     public void StartGame()
     {
+        DontDestroyOnLoad(transform.gameObject);
         //GameManager.SetActive(true);
         SceneManager.LoadScene("Prueba");
+        audioData.Play(0);
     }
 
     public void StartGacha()
     {
+        DontDestroyOnLoad(transform.gameObject);
+        
         //GameManager.SetActive(true);
         SceneManager.LoadScene("SampleScene");
+        audioData.Play(0);
     }
 
     //Cierra el juego
     public void Quit()
     {
+        audioData.Play(0);
         Application.Quit();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioData=GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
