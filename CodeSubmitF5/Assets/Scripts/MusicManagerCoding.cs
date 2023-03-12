644a4b037bee623053706b6060d129f56e0b6100
DDiegoP <95857593+DDiegoP@users.noreply.github.com>
2023-03-12 18:40:56 +0100

    Sonidos jogo

---
+++ CodeSubmitF5/Assets/Scripts/MusicManagerCoding.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManagerCoding : MonoBehaviour
+{
+    [SerializeField]
+    GameObject visualUI;
+
+    AudioSource audioSource;
+    AudioSource otherAudioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource= GetComponent<AudioSource>();
+        otherAudioSource= visualUI.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (visualUI.activeInHierarchy&&!otherAudioSource.isPlaying)
+        {
+            audioSource.Stop();
+            otherAudioSource.PlayDelayed(1);
+            
+        }
+        else if(!visualUI.activeInHierarchy&& !audioSource.isPlaying)
+        {
+            otherAudioSource.Stop();
+            audioSource.Play();
+        }
+    }
+}
